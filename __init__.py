00b3bbece51d4cefb6db7b3bd1fb24eb2fe7382b
Seth Trowbridge <seth111@gmail.com>
2024-11-30 07:35:25 -0500

    Update __init__.py
    blender 4.x tweaks

--- __init__.py
+++ __init__.py
@@ -1,12 +1,12 @@
 bl_info = {
     "name": "Crush Plane",
     "author": "Seth Trowbidge",
-    "version": (1, 0, 0),
-    "blender": (2, 7, 8),
-    "location": "Edit Mesh > Tools > Crush Plane",
-    "description": ("Work with planes in edit mesh mode"),
-    "warning": "",  # used for warning icon and text in addons panel
-    "category": "Mesh"}
+    "version": (2, 1, 0),
+    "blender": (4, 0, 0),
+    "location": "View3D > Edit Mesh > Crush Plane",
+    "description": "Work with planes in edit mesh mode",
+    "warning": ""
+}
 
 import bpy
 import bmesh
@@ -15,118 +15,125 @@ from mathutils import Vector
 from mathutils import Matrix
 
 class CrushPlane:
-
     ProjectVector = Vector((0, 0, -1))
     CustomVector = Vector((0, 0, -1))
 
     DrawPlaneName = "CrushPlane Plane"
-    DrawPlane = False
+    DrawPlane = None
 
-    def SetupPlane():
-        CrushPlane.DrawPlane = bpy.data.objects.new(CrushPlane.DrawPlaneName, None )
-        CrushPlane.DrawPlane.empty_draw_type = 'IMAGE'
-        CrushPlane.DrawPlane.empty_image_offset = [-0.5, -0.5]
-        CrushPlane.DrawPlane.empty_draw_size = 3
+    @classmethod
+    def SetupPlane(cls):
+        cls.DrawPlane = bpy.data.objects.new(cls.DrawPlaneName, None)
+        cls.DrawPlane.empty_display_type = 'IMAGE'
+        cls.DrawPlane.empty_image_offset = [-0.5, -0.5]
+        cls.DrawPlane.empty_display_size = 3
         
-    def ShowPlane():
-        bpy.context.scene.objects.link(CrushPlane.DrawPlane)
+    @classmethod
+    def ShowPlane(cls):
+        bpy.context.collection.objects.link(cls.DrawPlane)
     
-    def HidePlane():
-        bpy.context.scene.objects.unlink(CrushPlane.DrawPlane)
+    @classmethod
+    def HidePlane(cls):
+        bpy.context.collection.objects.unlink(cls.DrawPlane)
 
-
-    def GetNormal():
-        members = CrushPlane.DrawPlane.matrix_world.col
+    @classmethod
+    def GetNormal(cls):
+        members = cls.DrawPlane.matrix_world.col
         normal = Vector((members[2][0], members[2][1], members[2][2]))
         normal.normalize()
         return normal
     
-    def GetPosition():
-        members = CrushPlane.DrawPlane.matrix_world.col
+    @classmethod
+    def GetPosition(cls):
+        members = cls.DrawPlane.matrix_world.col
         return Vector((members[3][0], members[3][1], members[3][2]))
 
-    def SetPlane(inObject):
+    @classmethod
+    def SetPlane(cls, inObject):
         three = []
         mesh = bmesh.from_edit_mesh(inObject.data)
         for vert in mesh.verts:
             if vert.select:
-                three.append(inObject.matrix_world * vert.co)
-                if(len(three) == 3):
+                three.append(inObject.matrix_world @ vert.co)
+                if len(three) == 3:
                     
                     three[1] = three[1] - three[0]
                     three[2] = three[2] - three[0]
                     
-                    CrushPlane.PlaneVector = three[1].cross(three[2])
-                    CrushPlane.PlaneVector.normalize()
-                    CrushPlane.DrawPlane.matrix_world = Matrix.Translation(three[0])
+                    cls.PlaneVector = three[1].cross(three[2])
+                    cls.PlaneVector.normalize()
+                    cls.DrawPlane.matrix_world = Matrix.Translation(three[0])
                     
-                    vZ = CrushPlane.PlaneVector
+                    vZ = cls.PlaneVector
                     vY = three[1]
                     vY.normalize()
-                    vX = three[1].cross(CrushPlane.PlaneVector)
+                    vX = three[1].cross(cls.PlaneVector)
                     
-                    CrushPlane.DrawPlane.matrix_world.col[2][0] = vZ.x
-                    CrushPlane.DrawPlane.matrix_world.col[2][1] = vZ.y
-                    CrushPlane.DrawPlane.matrix_world.col[2][2] = vZ.z
+                    cls.DrawPlane.matrix_world.col[2][0] = vZ.x
+                    cls.DrawPlane.matrix_world.col[2][1] = vZ.y
+                    cls.DrawPlane.matrix_world.col[2][2] = vZ.z
                     
-                    CrushPlane.DrawPlane.matrix_world.col[1][0] = vY.x
-                    CrushPlane.DrawPlane.matrix_world.col[1][1] = vY.y
-                    CrushPlane.DrawPlane.matrix_world.col[1][2] = vY.z
+                    cls.DrawPlane.matrix_world.col[1][0] = vY.x
+                    cls.DrawPlane.matrix_world.col[1][1] = vY.y
+                    cls.DrawPlane.matrix_world.col[1][2] = vY.z
                     
-                    CrushPlane.DrawPlane.matrix_world.col[0][0] = -vX.x
-                    CrushPlane.DrawPlane.matrix_world.col[0][1] = -vX.y
-                    CrushPlane.DrawPlane.matrix_world.col[0][2] = -vX.z
+                    cls.DrawPlane.matrix_world.col[0][0] = -vX.x
+                    cls.DrawPlane.matrix_world.col[0][1] = -vX.y
+                    cls.DrawPlane.matrix_world.col[0][2] = -vX.z
                     
                     return
-        print("Not Enough Verticies selcted for plane")
+        print("Not Enough Vertices selected for plane")
 
-    def SetProjection(inObject):
+    @classmethod
+    def SetProjection(cls, inObject):
         two = []
         mesh = bmesh.from_edit_mesh(inObject.data)
         for vert in mesh.verts:
             if vert.select:
-                two.append(inObject.matrix_world * vert.co)
-                if(len(two) == 2):
-                    CrushPlane.CustomVector = two[1] - two[0]
+                two.append(inObject.matrix_world @ vert.co)
+                if len(two) == 2:
+                    cls.CustomVector = two[1] - two[0]
                     return
                 
-        print("Not Enough Verticies selcted for projection")
+        print("Not Enough Vertices selected for projection")
         
-    def SetPlaneOffset(inObject):
+    @classmethod
+    def SetPlaneOffset(cls, inObject):
         mesh = bmesh.from_edit_mesh(inObject.data)
         for vert in mesh.verts:
             if vert.select:
-                one = inObject.matrix_world * vert.co
+                one = inObject.matrix_world @ vert.co
                 
-                CrushPlane.DrawPlane.matrix_world.col[3][0] = one.x
-                CrushPlane.DrawPlane.matrix_world.col[3][1] = one.y
-                CrushPlane.DrawPlane.matrix_world.col[3][2] = one.z
+                cls.DrawPlane.matrix_world.col[3][0] = one.x
+                cls.DrawPlane.matrix_world.col[3][1] = one.y
+                cls.DrawPlane.matrix_world.col[3][2] = one.z
                 
-                CrushPlane.PlaneOffset = one.dot(CrushPlane.PlaneVector)
+                cls.PlaneOffset = one.dot(cls.PlaneVector)
                 return
 
-    #####
-
-    def CrushVerticies(inObject): 
+    @classmethod
+    def CrushVerticies(cls, inObject): 
         inverse = inObject.matrix_world.inverted()
-        normal = CrushPlane.GetNormal()
-        offset = CrushPlane.GetPosition().dot(normal)
-
         mesh = bmesh.from_edit_mesh(inObject.data)
         for vert in mesh.verts:
             if vert.select:
                 
-                vWorld = inObject.matrix_world * vert.co # get the world coords of the vert
-                pWorld = vWorld.dot(normal) - offset # project it onto the plane normal (with offset)
-                dWorld = vWorld - normal*pWorld # move the vert along the normal by the projection amount
-
-                vert.co = inverse * dWorld # commit the update in local coords
+                normal = cls.GetNormal()
+                offset = cls.GetPosition().dot(normal)
+                
+                worldSpace = inObject.matrix_world @ vert.co
+                vOther = worldSpace + cls.ProjectVector
+                dVertex = worldSpace.dot(normal) - offset
+                dOther = vOther.dot(normal) - offset
+                dSum = dVertex - dOther
+                dPercent = dVertex/dSum
+                worldSpace.x = worldSpace.x + dPercent*(vOther.x - worldSpace.x)
+                worldSpace.y = worldSpace.y + dPercent*(vOther.y - worldSpace.y)
+                worldSpace.z = worldSpace.z + dPercent*(vOther.z - worldSpace.z)
+                
+                vert.co = inverse @ worldSpace
                 
         bmesh.update_edit_mesh(inObject.data)
-
-###########################
-
-
 
 
 class CrushPlaneSetPlane(bpy.types.Operator):
@@ -160,13 +167,13 @@ class CrushPlaneSetProjection(bpy.types.Operator):
         return {'FINISHED'}
 
 class CrushPlaneCrushCustom(bpy.types.Operator):
-    """Crush verticies onto a plane along a direction"""
+    """Crush vertices onto a plane along a direction"""
     bl_idname = "mesh.crush_plane_crush_custom"
     bl_label = "Crush Plane: Crush"
     bl_options = {'REGISTER', 'UNDO'}
 
     def execute(self, context):
-        CrushPlane.ProjectVector = CrushPlane.CustomVector.copy();
+        CrushPlane.ProjectVector = CrushPlane.CustomVector.copy()
         CrushPlane.CrushVerticies(context.active_object)
         return {'FINISHED'}
 
@@ -213,7 +220,6 @@ class CrushPlaneBisect(bpy.types.Operator):
         bpy.ops.mesh.bisect(plane_co=CrushPlane.GetPosition(), plane_no=CrushPlane.GetNormal())
         return {'FINISHED'}
     
-
 class CrushPlaneSetupPlane(bpy.types.Operator):
     """Generate cut plane object"""
     bl_idname = "mesh.crush_plane_setup_plane"
@@ -244,29 +250,24 @@ class CrushPlaneHidePlane(bpy.types.Operator):
         CrushPlane.HidePlane()
         return {'FINISHED'}
     
-########
-
 class CrushPlaneUI(bpy.types.Panel):
-
-    bl_space_type = "VIEW_3D"
-    bl_region_type = "TOOLS"
-    bl_label = "Crush Plane"
-    bl_idname = "VIEW3D_PT_crush_plane"
-    bl_context = "mesh_edit"
-    bl_category = "Tools"
+    bl_space_type = 'VIEW_3D'
+    bl_region_type = 'UI'
+    bl_category = 'Crush'
+    bl_label = 'Crush Plane Tools'
 
     def draw(self, context):
-        
         layout = self.layout
         obj = context.object
-
-        if(CrushPlane.DrawPlaneName not in bpy.data.objects):
+        
+        
+        if CrushPlane.DrawPlaneName not in bpy.data.objects:
             col = layout.column(align=True)
             col.operator("mesh.crush_plane_setup_plane", text="Setup")
             
         else:
             col = layout.column(align=True)
-            if(CrushPlane.DrawPlaneName not in bpy.context.scene.objects):
+            if CrushPlane.DrawPlaneName not in bpy.context.scene.collection.objects:
                 col.operator("mesh.crush_plane_show_plane", text="Show Plane")
             else:
                 col.operator("mesh.crush_plane_hide_plane", text="Hide Plane")
@@ -288,38 +289,33 @@ class CrushPlaneUI(bpy.types.Panel):
             col.operator("mesh.crush_plane_bisect", text="Bisect")
 
 
+
+classes = [
+    CrushPlaneSetupPlane,
+    CrushPlaneShowPlane,
+    CrushPlaneHidePlane,
+    CrushPlaneSetPlane,
+    CrushPlaneSetPlaneOffset,
+    CrushPlaneSetProjection,
+    CrushPlaneCrushCustom,
+    CrushPlaneCrushX,
+    CrushPlaneCrushY,
+    CrushPlaneCrushZ,
+    CrushPlaneBisect,
+    CrushPlaneUI
+]
+
+def classes_unregister():
+    for item in classes:
+        bpy.utils.unregister_class(item)
+
 def register():
-
-    bpy.utils.register_class(CrushPlaneSetupPlane)
-    bpy.utils.register_class(CrushPlaneShowPlane)
-    bpy.utils.register_class(CrushPlaneHidePlane)
-    bpy.utils.register_class(CrushPlaneSetPlane)
-    bpy.utils.register_class(CrushPlaneSetPlaneOffset)
-    bpy.utils.register_class(CrushPlaneSetProjection)
-    bpy.utils.register_class(CrushPlaneCrushCustom)
-    bpy.utils.register_class(CrushPlaneCrushX)
-    bpy.utils.register_class(CrushPlaneCrushY)
-    bpy.utils.register_class(CrushPlaneCrushZ)
-    bpy.utils.register_class(CrushPlaneBisect)
-    bpy.utils.register_class(CrushPlaneUI)
-
+    for item in classes:
+        bpy.utils.register_class(item)
 
 def unregister():
-    
-    bpy.utils.unregister_class(CrushPlaneSetupPlane)
-    bpy.utils.unregister_class(CrushPlaneShowPlane)
-    bpy.utils.unregister_class(CrushPlaneHidePlane)
-    bpy.utils.unregister_class(CrushPlaneSetPlane)
-    bpy.utils.unregister_class(CrushPlaneSetPlaneOffset)
-    bpy.utils.unregister_class(CrushPlaneSetProjection)
-    bpy.utils.unregister_class(CrushPlaneCrushCustom)
-    bpy.utils.unregister_class(CrushPlaneCrushX)
-    bpy.utils.unregister_class(CrushPlaneCrushY)
-    bpy.utils.unregister_class(CrushPlaneCrushZ)
-    bpy.utils.unregister_class(CrushPlaneBisect)
-    bpy.utils.unregister_class(CrushPlaneUI)
-
-
+    for item in classes:
+        bpy.utils.unregister_class(item)
 
 if __name__ == "__main__":
-    register()  
+    register()
